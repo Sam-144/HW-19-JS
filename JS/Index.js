c18abe122efb34a065497aad0bc3e0ccaca9e139
Sam-144 <145414240+Sam-144@users.noreply.github.com>
2023-10-16 22:16:05 -0500

    Animate buttons

--- JS/Index.js
+++ JS/Index.js
@@ -5,6 +5,19 @@ function fade() {
 }
 setTimeout(fade, 3000)
 
-$(".order").on("click", function(){
-    $(".order").animate({height: "100px"});
+$(".herobutton").on({
+    mouseenter: function(){
+    $(".herobutton").animate({height: "100px", width: "300px"})
+    },
+    mouseleave: function(){
+        $(".herobutton").animate({height: "77px", width: "272px"})
+    }
+});
+$(".order").on({
+    mouseenter: function(){
+    $(".order").animate({height: "100px", width: "300px"})
+    },
+    mouseleave: function(){
+        $(".order").animate({height: "77px", width: "272px"})
+    }
 });
